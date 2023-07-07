3050d03a376680a037a3a8dd981d969b82523a13
guneykilicel <kilicelguney@gmail.com>
2023-07-07 15:53:07 +0300

    iskelet, FormGroup ve FormControl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CardformComponent } from './cardform/cardform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardformComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cardform/cardform.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-cardform',
+  templateUrl: './cardform.component.html',
+  styleUrls: ['./cardform.component.css']
+})
+export class CardformComponent {
+  cardForm = new FormGroup({
+    name: new FormControl(''),
+    
+  })
+}
