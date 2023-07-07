267a745f29ce877e3667e5a3273b07e25eb0b4d8
guneykilicel <kilicelguney@gmail.com>
2023-07-07 17:47:46 +0300

    diger inputlar ve buton eklendi onSubmit ve disabl

--- src/app/cardform/cardform.component.ts
+++ src/app/cardform/cardform.component.ts
@@ -12,9 +12,27 @@ export class CardformComponent {
       Validators.required,
       Validators.minLength(3),
     ]),
+    cardNumber: new FormControl(null),
+    expiration: new FormControl(null),
+    securityCode: new FormControl(null),
   });
+
+  onSubmit() {
+    
+  }
 
   get nameControl() {
     return this.cardForm.get('name') as FormControl;
   }
+  get cardNumberControl() {
+    return this.cardForm.get('cardNumber') as FormControl;
+  }
+  get expirationControl() {
+    return this.cardForm.get('expiration') as FormControl;
+  }
+  get securityCodeControl() {
+    return this.cardForm.get('securityCode') as FormControl;
+  }
+
+
 }
